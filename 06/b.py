6100d011503a1faa24e965e1301a433ab133d23b
Eric Harley <eric.harley@gmail.com>
2021-12-06 19:49:12 -0500

    Update b.py

--- 06/b.py
+++ 06/b.py
@@ -11,20 +11,24 @@ for i in range(9):
   b[i] = np.sum( a == i )
 
 A = np.zeros((9,9), dtype=int)
-A[1,0] = 1
-A[2,1] = 1
-A[3,2] = 1
-A[4,3] = 1
-A[5,4] = 1
-A[6,5] = 1
-A[7,6] = 1
-A[8,7] = 1
-A[0,8] = 1
-A[0,6] = 1
-A = A.transpose()
+# A[to, from] = multiplier
+# each fish clock goes from N to N-1
+A[0,1] = 1
+A[1,2] = 1
+A[2,3] = 1
+A[3,4] = 1
+A[4,5] = 1
+A[5,6] = 1
+A[6,7] = 1
+A[7,8] = 1
+# fish at zero go to 6 and produce new fish at 8
+A[6,0] = 1
+A[8,0] = 1
 
+# part 1
 num_steps = 80
 print(np.sum( np.linalg.matrix_power(A, num_steps) @ b ) )
 
+# part 2
 num_steps = 256
 print(np.sum( np.linalg.matrix_power(A, num_steps) @ b ) )
