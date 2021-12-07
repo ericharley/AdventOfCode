cd5666f022511d8a55d50b26889a5ad7b0dc0a85
Eric Harley <eric.harley@gmail.com>
2021-12-07 09:27:49 -0500

    Day 7

---
+++ 07/a.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+# data = np.loadtxt("test.txt", dtype=int, delimiter=",")
+data = np.loadtxt("input.txt", dtype=int, delimiter=",")
+
+# Part 1
+# Geometric median, Weber problem
+v = np.median(data)
+print( int(np.sum(abs(data - v))) )
+
+# Part 2
+# Solution is near the mean +/- 0.5
+w = lambda x: (x * (x+1)//2)
+f = lambda x: int( w(abs(data-x)).sum() )
+
+a = np.mean(data)
+v1 = int(np.floor(a))
+v2 = int(np.ceil(a))
+
+if f(v1) < f(v2):
+    print(f(v1))
+else:
+    print(f(v2))
