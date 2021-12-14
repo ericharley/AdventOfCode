38558d3bb8c867a7997e99271214552de343a26e
Eric Harley <eric.harley@gmail.com>
2021-12-14 09:04:08 -0500

    Update b.py

--- 14/b.py
+++ 14/b.py
@@ -17,26 +17,15 @@ def doit(n):
  f = {}
  for i in range(len(input)-1):
    pair = input[i:i+2]
-   if pair in f:
-     f[pair] += 1
-   else:
-     f[pair] = 1
+   f[pair] = f.get(pair, 0) + 1
  
  for i in range(n):
    out = {}
    for pair in f:
      pair1 = pair[0] + m[pair]
      pair2 = m[pair] + pair[1]
-     if pair1 in out:
-       out[pair1] += f[pair]
-     else:
-       out[pair1] = f[pair]
-   
-     if pair2 in out:
-       out[pair2] += f[pair]
-     else:
-       out[pair2] = f[pair]
-   
+     out[pair1] = out.get(pair1, 0) + f[pair]
+     out[pair2] = out.get(pair2, 0) + f[pair]
    f = out
 
  freq = {}
@@ -45,6 +34,7 @@ def doit(n):
    for k in f:
      if ch in k[0]:
         freq[ch] += f[k]
+ #freq = { ch : sum([f[k] for k in f.keys() if ch == k[0]]) for ch in set(''.join(f.keys())) }
         
  freq[last_ch] += 1
 
