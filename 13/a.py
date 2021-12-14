9f7a389c53705b8828dd2d5d13dcfd702a67fb59
Eric Harley <eric.harley@gmail.com>
2021-12-13 19:42:22 -0500

    Day 13

--- 13/a.py
+++ 13/a.py
@@ -13,17 +13,14 @@ for line in lines:
  if line.startswith('fold'):
    (_,_,d) = line.split(' ')
    (d,v) = d.split('=')
-   v = int(v)
-   ins.append([d,v])
+   ins.append([d,int(v)])
  else:
    (x,y) = line.split(',')
-   x = int(x)
-   y = int(y)
-   paper[y,x] = 1
+   paper[int(y),int(x)] = 1
 
 part1 = True
-for op in ins:
-   (d,v) = op
+
+for (d,v) in ins:
    if d == 'y':
      top = paper[0:v,:]
      bottom = np.flipud(paper[v+1:,:])
@@ -31,19 +28,20 @@ for op in ins:
        top    = np.pad(top, pad_width=((np.shape(bottom)[0] - np.shape(top)[0], 0),(0,0)))
      else:
        bottom = np.pad(bottom, pad_width=((np.shape(top)[0] - np.shape(bottom)[0], 0),(0,0)))
-     res = top + bottom
-     paper = res
-   elif d =='x':
-     top = paper[:,0:v]
-     bottom = np.fliplr(paper[:,v+1:])
-     res = top + bottom
-     paper = res
+     paper = top + bottom
 
+   elif d =='x':
+     paper = paper[:,0:v] + np.fliplr(paper[:,v+1:])
+
+# Part 1
+# Print number of dots after first fold
    if part1:
      paper[paper > 1] = 1
      print((np.sum(paper)))
      part1 = False
 
+# Part 2
+# Print Result
 for r in range(6):
  for c in range(40):
   if int(paper[r,c]) >= 1:
