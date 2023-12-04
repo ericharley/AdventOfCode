575e2db576e1ba8d1e6f2cfb12b27945f241819e
Eric Harley <eric.harley@gmail.com>
2023-12-04 14:10:51 -0500

    Update e.py

--- 2023/04/e.py
+++ 2023/04/e.py
@@ -4,34 +4,25 @@ from collections import defaultdict
 filename = 'input.txt'
 lines = [line for line in open(filename).read().splitlines()]
 
-# weights
-w = defaultdict(lambda : 0)
 # card counts
-c = defaultdict(lambda : 0)
+c = defaultdict(int)
+
 # score for part 1
 total = 0
 
-
-for line in lines:
-  line = line.split()
-  pipe_idx = line.index('|')
-  max_n = pipe_idx - 2 # how many winning numbers do cards have?
-  k = int(line[1][:-1]) # take off the trailing ':'
-  a = set([int(x) for x in line[2:(pipe_idx)]]) # winning numbers
-  b = set([int(x) for x in line[(pipe_idx+1):]])# revealed numbers
+for k, line in enumerate(lines):
+  _, right = line.split(': ')
+  a_str, b_str = right.split(' | ')
+  a = set(a_str.split())
+  b = set(b_str.split())
   n = len(a & b) # how many revealed numers are winning
 
   # Part 1
-  total += round(2**(n-1)) # NB: python rounds 0.5 to 0, so works for when n = 0
+  total += 2**(n-1) if n != 0 else 0
 
   # Part 2
-  for i in range(1, n+1):
-    w[(k,k+i)] += 1
+  c[k] += 1
+  for i in range(1,n+1):
+    c[k+i] += c[k]
 
-  c[k] = 1
-  for i in range(1,max_n+1):
-    c[k] += w[(k-i,k)]*c[k-i]
-
-
-print(total)
-print(sum(c.values()))
+print( total, sum(c.values()) )
