933b91c3dec4401f0eb5c144b98df74650dba49e
Eric Harley <eric.harley@gmail.com>
2023-12-04 14:25:24 -0500

    Update a.py

--- 2023/04/a.py
+++ 2023/04/a.py
@@ -1,36 +1,24 @@
-from collections import defaultdict
-import re
-
 filename = 'input.txt'
-#filename = 'test.txt'
-
-lines = [line.split(': ') for line in open(filename).read().splitlines()]
+lines = [line.split(': ')[1].split('|') for line in open(filename).read().splitlines()]
 
 deck = {}
 hand = {}
 
 total = 0
-for line in lines:
-  k = int(''.join([x for x in line[0] if x.isdigit()]))
-  a,b = line[1].split(' | ')
-  a = set([int(x) for x in a.split(' ') if x != ''])
-  b = set([int(x) for x in b.split(' ') if x != ''])
+for k, line in enumerate(lines):
+  a = set(line[0].split())
+  b = set(line[1].split())
   n = len(a & b)
-  v = round(2**(n-1))
+
+  # Part 1
+  total += 2**(n-1) if n > 0 else 0
+
+  # Part 2
   deck[k] = n
   hand[k] = 1
-  total += v
-print(total)
 
-def process_card(i):
-  n = deck[i]
-  for j in range(1,n+1):
-    hand[i+j] += 1
+for i in hand:
+  for j in range(deck[i]):
+    hand[i+j+1] += hand[i]
 
-N = len(hand)
-for i in range(1,N+1):
-  for _ in range(hand[i]):
-    process_card(i)
-
-score = sum(hand.values())
-print(score)
+print(total, sum(hand.values()))
