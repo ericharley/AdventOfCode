66c55a3c309c14f9b7a7655bf22b702b97a5fb1a
Eric Harley <eric.harley@gmail.com>
2024-12-08 14:35:04 -0500

    Day 8

--- 2024/08/a.py
+++ 2024/08/a.py
@@ -1,28 +1,22 @@
 from itertools import combinations
 
 grid = open('input.txt').read().splitlines()
-
-board  = {(r,c) : grid[r][c] for r,row in enumerate(grid) for c,col in enumerate(row)}
-groups = {v : [k for k in board if board[k] == v] for v in board.values() if v != '.'}
+board  = {complex(r,c) : ch for r,row in enumerate(grid) for c,ch in enumerate(row) }
+groups = {v : [k for k in board if board[k] == v] for v in board.values() if v!='.' }
 
 # Part 1
-antinodes = set()
+antinodes, antinodes_2 = set(), set()
 
 for k in groups:
-  for (x1,y1),(x2,y2) in combinations(groups[k], 2):
-    dx,dy = x2-x1,y2-y1
-    antinodes.add( (x1-dx,y1-dy) )
-    antinodes.add( (x2+dx,y2+dy) )
+  for z1,z2 in combinations(groups[k], 2):
+    d = z2 - z1
+    antinodes.update( [z1 - d, z2 + d] )
 
-print(len([a for a in antinodes if a in board]))
+    # Part 2
+    for z3 in board:
+      if (z1.conjugate()*z2 + z2.conjugate()*z3 + z3.conjugate()*z1).imag == 0 :
+        antinodes_2.add(z3)
 
-# Part 2
-antinodes = set()
+print( len(antinodes & set(board)) )
 
-for k in groups:
-  for (x1,y1),(x2,y2) in combinations(groups[k], 2):
-    for (x3,y3) in board:
-      if x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2) == 0:
-        antinodes.add((x3,y3))
-
-print(len(antinodes))
+print( len(antinodes_2) )
