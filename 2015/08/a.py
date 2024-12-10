ca25c1c1e4a1d635a297b04ae014354a5e1bbd8a
Eric Harley <eric.harley@gmail.com>
2024-12-10 12:41:14 -0500

    Update a.py

--- 2015/08/a.py
+++ 2015/08/a.py
@@ -1,10 +1,15 @@
+import ast
 import json
 
 lines = open('input.txt').read().strip().split('\n')
 
 a,b = 0,0
 for line in lines:
-  a += (len(line) - len(eval(line)))
+  a += (len(line) - len(ast.literal_eval(line)))
   b += len(json.dumps(line)) - len(line)
 
-print(a, b)
+# Part 1
+print(a)
+
+# Part 2
+print(b)
