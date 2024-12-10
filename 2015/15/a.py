5e93fdfd8dde6bdcad0dfbcd5cb219ab51a22a11
Eric Harley <eric.harley@gmail.com>
2024-12-10 12:42:05 -0500

    Update a.py

--- 2015/15/a.py
+++ 2015/15/a.py
@@ -1,9 +1,11 @@
 max_score = 0
 max_score_2 = 0
 
+i = 0
 for a in range(100 + 1):
  for b in range(100 - a + 1):
   for c in range(100 - a - b + 1):
+    i += 1
     d = 100 - a - b - c
     capacity = max(0,3*a + -3*b + -1*c)
     durability = max(0,3*b)
@@ -16,7 +18,6 @@ for a in range(100 + 1):
     calories = 2*a+9*b+1*c+8*d
     if calories == 500:
       max_score_2 = max(max_score_2, score)
-
 
 print(max_score)
 print(max_score_2)
